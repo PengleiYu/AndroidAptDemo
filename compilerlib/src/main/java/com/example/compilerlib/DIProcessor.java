8feef8a8e0d8c1840adfabba40f57f17038ffb28
yupenglei <yupengniaosanshi@gmail.com>
2019-01-30 14:08:06 +0800

    开始依赖注入

---
+++ compilerlib/src/main/java/com/example/compilerlib/DIProcessor.java
@@ -0,0 +1,23 @@
+package com.example.compilerlib;
+
+import com.google.auto.service.AutoService;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+
+@AutoService(Processor.class)
+public class DIProcessor extends AbstractProcessor{
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return super.getSupportedAnnotationTypes();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        return false;
+    }
+}
